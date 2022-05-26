eb97621288b40d8f79e7cb993ac208b049319ca2
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-05-26 23:02:14 +0200

    Add field again

--- books/models.py
+++ books/models.py
@@ -4,6 +4,9 @@ class Book(models.Model):
     external_id = models.CharField(blank=True, max_length=300)
     title = models.CharField(max_length=500)
     authors = models.CharField(max_length=500)
-    acquired = models.BooleanField(default=False)
     published_year = models.PositiveSmallIntegerField()
+    acquired = models.BooleanField(default=False)
     thumbnail = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"Book - {self.title} - {self.authors}"
