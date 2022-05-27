f048dfcb36c9fcef7634543fd964f716e9e2ef88
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-05-27 10:41:06 +0200

    Change authors field to Array

--- books/models.py
+++ books/models.py
@@ -1,12 +1,14 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 class Book(models.Model):
     external_id = models.CharField(blank=True, max_length=300)
     title = models.CharField(max_length=500)
-    authors = models.CharField(max_length=500)
+    authors = ArrayField(
+        models.CharField(max_length=200))
     published_year = models.PositiveSmallIntegerField()
     acquired = models.BooleanField(default=False)
     thumbnail = models.URLField(blank=True)
 
     def __str__(self):
-        return f"Book - {self.title} - {self.authors}"
+        return f"Book - {self.title} {self.authors}"
